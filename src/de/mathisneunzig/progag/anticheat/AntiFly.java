404216c34508e12cbc1f174f32ac29622c831b3e
Intence <lassetv2001@gmail.com>
2016-07-23 19:37:45 +0200

    AntiFly

---
+++ src/de/mathisneunzig/progag/anticheat/AntiFly.java
@@ -0,0 +1,16 @@
+package de.mathisneunzig.progag.anticheat;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class AntiFly implements Listener {
+	
+	@EventHandler
+	public void onFly(PlayerMoveEvent e) {
+		
+		
+		
+	}
+	
+}
